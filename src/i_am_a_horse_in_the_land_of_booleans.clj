9a3d9165298ac398cd78094f347de374b7e1c01d
Lauri Kinnunen <lkkinnunen@gmail.com>
2014-01-05 16:06:23 +0200

    1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,28 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
   ":(")
 
+(defn divisible? [divisor n]
+  (zero? (mod n divisor)))
+
 (defn divides? [divisor n]
-  ":(")
+  (if (zero? (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divisible? 15 n) "gotcha!"
+    (divisible? 3 n) "fizz"
+    (divisible? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 13 age 20))
 
 (defn not-teen? [age]
   ":(")
@@ -26,3 +35,15 @@
   ":(")
 
 ; '_______'
+
+
+(boolean nil)
+
+(divides? 2 5)
+
+(teen? 15)
+
+
+(divisible? 4 2)
+
+(fizzbuzz 70)
